0dcc449ac6657aae44c03b69f92f848da7e535ef
Adriano Santos <adrianodrix@gmail.com>
2020-03-24 18:54:11 -0300

    feat: finish api

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,10 @@ const routes = express.Router();
 
 const ONGController = require('./controllers/ONGController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', ONGController.index);
 routes.post('/ongs', ONGController.create);
@@ -10,5 +14,7 @@ routes.post('/ongs', ONGController.create);
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes;
